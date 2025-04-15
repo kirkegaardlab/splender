143604ebc2d2821667d40bae542dcb5b14af0564
Frans <frzdyb@gmail.com>
2025-04-15 16:25:12 +0200

    equinox messes up param freezing

--- splender/image2.py
+++ splender/image2.py
@@ -14,6 +14,7 @@ import optax
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import equinox as eqx
+import jax.tree_util as jtu
 
 class Splender(eqx.Module):
     """
@@ -21,6 +22,7 @@ class Splender(eqx.Module):
     This class is not meant to be used directly.
     """
     res: int = 128
+    n_images: int = 1
     n_splines: int = 1
     s_knots: int = 7
     n_points_per_spline_per_frame: int = 100
@@ -50,12 +52,13 @@ class Splender(eqx.Module):
         self.spline_brightness = jnp.zeros((1,))
         if init_knots is not None:
             init_knot_params = logit(init_knots)
-            init_param_mean = init_knot_params.mean(1, keepdims=True)
+            init_param_mean = init_knot_params.mean(-2, keepdims=True)
             init_params = init_knot_params - init_param_mean
-            self.n_splines = init_knot_params.shape[0]
-            self.s_knots = init_knot_params.shape[1]
-            self.loc_params = jnp.concatenate([init_param_mean, 5 * jnp.ones((self.n_splines, 1, 1))], axis=2)
-            self.knot_params = jnp.concatenate([init_params, jnp.zeros((self.n_splines, self.s_knots, 1))], axis=2)
+            self.n_images = init_knot_params.shape[0]
+            self.n_splines = init_knot_params.shape[1]
+            self.s_knots = init_knot_params.shape[2]
+            self.loc_params = jnp.concatenate([init_param_mean, 5 * jnp.ones((self.n_images, self.n_splines, 1, 1))], axis=-1)
+            self.knot_params = jnp.concatenate([init_params, jnp.zeros((self.n_images, self.n_splines, self.s_knots, 1))], axis=-1)
         else:            
             self.loc_params = jnp.zeros((self.n_splines, 1, 3))
             self.knot_params = jnp.zeros((self.n_splines, self.s_knots, 3))
@@ -73,7 +76,7 @@ class Splender(eqx.Module):
         x = jnp.linspace(-1, 1, self.brush_profile.shape[0])
         spline = Interpolator1D(x, self.brush_profile, method='cubic')
         X, Y = jnp.meshgrid(x, x)
-        D = jnp.sqrt(X**2 + Y**2)
+        D = jnp.sqrt(X**2 + Y**2 + self.eps)
         brush_image = vmap(spline)(jnp.exp(-D))
         return brush_image
     
@@ -88,18 +91,7 @@ class Splender(eqx.Module):
             brush_image, (self.res, self.res), (0, 1),
             scale * jnp.ones(2), point_mapped_to_image, method='cubic')
     
-    def cumulative_spline_length(self, x_spline, y_spline):
-        # Compute arc length
-        dx_ds = vmap(partial(x_spline, dx=1))
-        dy_ds = vmap(partial(y_spline, dx=1))
-        s_fine = jnp.linspace(0, 1, self.n_points_per_spline_per_frame)
-        ds_vals = jnp.sqrt(dx_ds(s_fine)**2 + dy_ds(s_fine)**2 + self.eps)
-        delta_s = s_fine[1] - s_fine[0]
-        cumulative_length = jnp.concatenate([
-            jnp.array([0.0]),
-            jnp.cumsum(0.5 * (ds_vals[1:] + ds_vals[:-1]) * delta_s)
-        ])
-        return cumulative_length
+
 
     def get_uniform_points(self, x_spline, y_spline):
         # Compute cumulative spline length
@@ -114,11 +106,39 @@ class Splender(eqx.Module):
     def fit_spline(self):
         raise NotImplementedError("This method should be implemented in subclasses.")
     
+    def spatial_derivative(self, spline, degree = 1):
+        raise NotImplementedError("This method should be implemented in subclasses.")
+    
+    def cumulative_spline_length(self, x_spline, y_spline):
+        # Compute arc length
+        dx_ds = vmap(self.spatial_derivative(x_spline, degree=1))
+        dy_ds = vmap(self.spatial_derivative(y_spline, degree=1))
+        s_fine = jnp.linspace(0, 1, self.n_points_per_spline_per_frame)
+        ds_vals = jnp.sqrt(dx_ds(s_fine)**2 + dy_ds(s_fine)**2 + self.eps)
+        delta_s = s_fine[1] - s_fine[0]
+        cumulative_length = jnp.concatenate([
+            jnp.array([0.0]),
+            jnp.cumsum(0.5 * (ds_vals[1:] + ds_vals[:-1]) * delta_s)
+        ])
+        return cumulative_length
+    
+    def mean_spline_curvature(self, x_spline, y_spline):
+        d2x_ds2 = vmap(self.spatial_derivative(x_spline, degree=2))
+        d2y_ds2 = vmap(self.spatial_derivative(y_spline, degree=2))
+        s_fine = jnp.linspace(0, 1, self.n_points_per_spline_per_frame)
+        delta_s = s_fine[1] - s_fine[0]
+        curvature = jnp.sqrt(d2x_ds2(s_fine)**2 + d2y_ds2(s_fine)**2 + self.eps) * delta_s
+        return curvature.mean(0)
+
+    
     def __call__(self):
         NotImplementedError("This method should be implemented in subclasses.")
 
 
 class SplenderImage(Splender):
+
+    def spatial_derivative(self, spline, degree = 1):
+        return partial(spline, dx=degree)
 
     def fit_spline(self, knot_params):
 
@@ -153,7 +173,7 @@ class SplenderImage(Splender):
                     )
         return image
     
-    def render_splines(self):
+    def render_splines(self, knots):
         """
         Render all splines in a separate image.
         If knot_params are all zeros, the image is empty.
@@ -161,24 +181,26 @@ class SplenderImage(Splender):
         """
         def maybe_render_spline(knot_params):
             def render_empty():
-                return jnp.zeros((self.res, self.res))
+                return jnp.zeros((self.res, self.res)), 0., 0.
             
             def render_spline():
                 x_spline, y_spline, scale_spline = self.fit_spline(knot_params)
-                return self.render_spline(x_spline, y_spline, scale_spline)
+                length = self.cumulative_spline_length(x_spline, y_spline)[-1]
+                curvature = self.mean_spline_curvature(x_spline, y_spline)
+                return self.render_spline(x_spline, y_spline, scale_spline), length, curvature
             
             return jax.lax.cond(jnp.all(knot_params[:2] == 0), render_empty, render_spline)
         
-        knots = self.loc_params + self.knot_params
-        spline_images = vmap(maybe_render_spline)(knots)
-
+        spline_images, lengths, curvatures = vmap(maybe_render_spline)(knots)
+        
         # aggregate spline_images
         splines_image = jnp.max(spline_images, axis=0) * self.opacity + jnp.sum(spline_images, axis=0) * (1 - self.opacity)
 
-        return splines_image
+        return splines_image, lengths, curvatures
     
-    def render_image(self):
-        splines_image = self.render_splines() * self.spline_contrast + self.spline_brightness
+    def render_image(self, knots):
+        splines_image, lengths, curvatures = self.render_splines(knots)
+        # splines_image = splines_image * self.spline_contrast + self.spline_brightness
 
         image = splines_image #+ self.logistic_background()
 
@@ -186,8 +208,9 @@ class SplenderImage(Splender):
 
         image = image * self.contrast + self.brightness
         image = sigmoid(image)
-        return image
+        return image, lengths, curvatures
     
     def __call__(self):
-        image = self.render_image()
-        return image
+        knots = self.loc_params + self.knot_params
+        images, lengths, curvatures = jax.vmap(self.render_image)(knots)
+        return images, lengths, curvatures
